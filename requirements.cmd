b23757d2ff514cb28c4cfbc7a5b85320e192dc7e
Bijo <lilgreeze@gmail.com>
2022-12-26 18:37:38 +0400

    add requirements.cmd

---
+++ requirements.cmd
@@ -0,0 +1,16 @@
+@echo off
+
+:start
+cls
+
+pip install colorama
+pip install requests
+pip install selenium
+pip install undetected-chromedriver
+pip install beautifulsoup4
+pip install pandas
+pip install latest_user_agents
+pip install winotify
+
+pause
+exit
